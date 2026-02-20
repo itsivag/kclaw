aa5c8ec88d60e86adcb3eb0f6c8cb20421ee319c
itsivag <sivacbrf2@gmail.com>
2026-02-20 16:56:46 +0530

    Add JetBrains koog dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.10"
+    kotlin("plugin.serialization") version "2.2.10"
     application
 }
 
@@ -12,6 +13,9 @@ repositories {
 
 dependencies {
     implementation("com.github.ajalt.clikt:clikt:5.1.0")
+    implementation("ai.koog:koog-agents:0.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
     testImplementation(kotlin("test"))
 }
 
